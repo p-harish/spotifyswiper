e9b0c923fac41bb630a7a4d6d25c447e3e2c70e1
Harish Parthasarathy <hparthas@uwaterloo.ca>
2022-05-31 10:16:17 -0400

    removed update

--- app/src/main/java/me/harishpartha/spotifyswiper/Swipe.java
+++ app/src/main/java/me/harishpartha/spotifyswiper/Swipe.java
@@ -113,31 +113,58 @@ public class Swipe extends AppCompatActivity {
     }
 
     private void updateRecommendationStack() {
-        ArrayList<ImagesModal> songCardArrayNew = new ArrayList<>();
-        Deque<Recommendation> recommendationsNew = recommendations;
+//        ArrayList<ImagesModal> songCardArrayNew = new ArrayList<>();
+//        Deque<Recommendation> recommendationsNew = recommendations;
+//
+//        Recommendation[] spotifyArray = Spotify.getRecommendationsArray();
+//        for (int i = 0; i < Spotify.getRecommendationsFilled(); i++) {
+//            Recommendation r = spotifyArray[i];
+//            songCardArray.add(new ImagesModal(r.getName(), r.getAlbum(), r.getArtist(), r.getCover()));
+//            Log.e("song", r.getUrl());
+//        }
+//
+//        final SongStack adapter = new SongStack(songCardArray, this);
+//        cardStack.setAdapter(adapter);
+//        cardStack.setEventCallback(new SwipeDeck.SwipeEventCallback() {
+//            @Override
+//            public void cardSwipedLeft(int position) {
+//                recommendations.pop();
+//                Spotify.playSong(recommendations.peek().getUrl());
+//                Log.e("song", recommendations.peek().getUrl());
+//                if (refreshStack)
+//                    updateRecommendationStack();
+//            }
+//
+//            @Override
+//            public void cardSwipedRight(int position) {
+//                Spotify.addSong(recommendations.peek().getUrl());
+//                recommendations.pop();
+//                Spotify.playSong(recommendations.peek().getUrl());
+//                Log.e("song", recommendations.peek().getUrl());
+//                Spotify.getFreshRecommendations();
+//                if (refreshStack)
+//                    updateRecommendationStack();
+//            }
+//
+//            @Override
+//            public void cardsDepleted() {
+//                Log.e("End", "At end of stack");
+//                done();
+//            }
+//
+//            @Override
+//            public void cardActionDown() {
+//                // this method is called when card is swipped down.
+//                Log.i("TAG", "CARDS MOVED DOWN");
+//            }
+//
+//            @Override
+//            public void cardActionUp() {
+//                // this method is called when card is moved up.
+//                Log.i("TAG", "CARDS MOVED UP");
+//            }
+//        });
 
-        Recommendation[] spotifyArray = Spotify.getRecommendationsArray();
-        for (int i = 0; i < Spotify.getRecommendationsFilled(); i++) {
-            Recommendation r = spotifyArray[i];
-            songCardArray.add(new ImagesModal(r.getName(), r.getAlbum(), r.getArtist(), r.getCover()));
-            Log.e("song", r.getUrl());
-        }
-
-
-        int recommendationsSize = recommendations.size();
-
-        Iterator<Recommendation> it = recommendations.iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
-        while (true) {
-            Recommendation r = spotifyArray[i];
-
-            songCardArray.add(new ImagesModal(r.getName(), r.getAlbum(), r.getArtist(), r.getCover()));
-
-        }
-
-        final SongStack adapter = new SongStack(songCardArray, this);
 
     }
 }
